b14335cdcbe8ab2f989ca79ba5e7beaed8ca8f1e
cristianve <97cvega@gmail.com>
2018-11-29 16:46:41 +0100

    Jeimmy

--- app/src/main/java/com/example/cvegasan/timetrackerapp/MainActivity.java
+++ app/src/main/java/com/example/cvegasan/timetrackerapp/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
+        //TEST
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
